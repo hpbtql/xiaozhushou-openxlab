cf2cc8dfb98c6b7ab204fa19e9a31dfe181d0fc2
hpbtql <86307756+hpbtql@users.noreply.github.com>
2024-04-17 21:26:08 +0800

    change some path and description

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer, AutoModel
 from openxlab.model import download
 
-base_path = './internlm2-chat-7b'
-os.system(f'git clone https://code.openxlab.org.cn/OpenLMLab/internlm2-chat-7b.git {base_path}')
+base_path = './xiaozhushou'
+os.system(f'git clone https://code.openxlab.org.cn/hpbtql/xiaozhushou.git {base_path}')
 os.system(f'cd {base_path} && git lfs pull')
 
 tokenizer = AutoTokenizer.from_pretrained(base_path,trust_remote_code=True)
@@ -16,8 +16,8 @@ def chat(message,history):
         yield response
 
 gr.ChatInterface(chat,
-                 title="InternLM2-Chat-7B",
+                 title="hpbtql的小助手",
                 description="""
-InternLM is mainly developed by Shanghai AI Laboratory.  
+这是经过hpbtqlFinetune以后的小助手 
                  """,
                  ).queue(1).launch()
